1a60ddbd7427443f4567b0b2337a7ed8099d4cc0
Deepak Jaggi <deepak.jaggi@banqsoft.no>
2023-06-11 20:16:05 +0200

    mock

--- senior-qa-challenge-main/cypress/e2e/tests/api.cy.js
+++ senior-qa-challenge-main/cypress/e2e/tests/api.cy.js
@@ -5,68 +5,6 @@ let testDataNorway;
 let testDataBerlin;
 let testDataPort;
 
-const mkedResponse = {
-    "coord": {
-        "lon": 11.1729,
-        "lat": 60.2839
-    },
-    "weather": [
-        {
-            "id": 800,
-            "main": "Clear",
-            "description": "clear sky",
-            "icon": "01d"
-        }
-    ],
-    "base": "stations",
-    "main": {
-        "temp": 22.17,
-        "feels_like": 21.49,
-        "temp_min": 21.08,
-        "temp_max": 22.74,
-        "pressure": 1024,
-        "humidity": 40
-    },
-    "visibility": 10000,
-    "wind": {
-        "speed": 5.14,
-        "deg": 190
-    },
-    "clouds": {
-        "all": 0
-    },
-    "dt": 1686490582,
-    "sys": {
-        "type": 2,
-        "id": 2006195,
-        "country": "NO",
-        "sunrise": 1686448247,
-        "sunset": 1686515950
-    },
-    "timezone": 7200,
-    "id": 3142539,
-    "name": "Råholt",
-    "cod": 400
-};
-
-// describe('Mock Server', () => {
-//     beforeEach(() => {
-//         cy.intercept('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=60.2838578&lon=11.1728733&units=Metric&appid=eb8a70f875f4e4baabc1399cec36e4b6', (req) => {
-//             req.reply({ message: JSON.stringify(mkedResponse) }); // Provide the mocked response
-//         }).as('getMockResponse');
-//     });
-
-//     it('should receive mocked response from the original API', () => {
-//         var url = "https://api.openweathermap.org/data/2.5/weather?lat=60.2838578&lon=11.1728733&units=Metric&appid=eb8a70f875f4e4baabc1399cec36e4b6"
-//         cy.request('GET', url).then((x) => {
-//             // Assert on the mocked response
-//             cy.wait('@getMockResponse').then((interception) => {
-//                 expect(interception.response.body.message).to.eq(JSON.stringify(mkedResponse));
-//             });
-//         })
-//     });
-
-// });
 
 describe('apiTests', function () {
 
---
+++ senior-qa-challenge-main/cypress/mock-server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/mockOslo', (req, res) => {
+
+
+    const mkedResponse = {
+        "coord": {
+            "lon": 11.1729,
+            "lat": 60.2839
+        },
+        "weather": [
+            {
+                "id": 800,
+                "main": "Clear",
+                "description": "clear sky",
+                "icon": "01d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 22.17,
+            "feels_like": 21.49,
+            "temp_min": 21.08,
+            "temp_max": 22.74,
+            "pressure": 1024,
+            "humidity": 40
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 5.14,
+            "deg": 190
+        },
+        "clouds": {
+            "all": 0
+        },
+        "dt": 1686490582,
+        "sys": {
+            "type": 2,
+            "id": 2006195,
+            "country": "NO",
+            "sunrise": 1686448247,
+            "sunset": 1686515950
+        },
+        "timezone": 7200,
+        "id": 3142539,
+        "name": "Råholt",
+        "cod": 400
+    };
+
+
+    res.json(mkedResponse);
+});
+
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Mock server is running on port ${port}`);
+});
